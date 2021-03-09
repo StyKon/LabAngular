d08878eda01b82793d1f65178b7c9b2179fdd101
StyKon <76910363+StyKon@users.noreply.github.com>
2021-03-09 14:45:10 +0100

    StyKonCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemberListComponent } from './member-list/member-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { MemberListComponent } from './member-list/member-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTableModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'cin', 'name', 'type', 'cv' , 'createdDate'];
 dataSource: any[] = [
   {
     id: '123',
     cin:  '11093299',
     name: 'khalil',
     type: 'teacher',
-    cv: '.pdf',
+    cv: 'cv1.pdf',
     createdDate: '02/03/2021'
   },
   {
@@ -20,7 +20,7 @@ dataSource: any[] = [
     cin:  '11093299',
     name: 'khalil',
     type: 'teacher',
-    cv: '.pdf',
+    cv: 'cv2.pdf',
     createdDate: '02/03/2021'
   },
   {
@@ -28,7 +28,7 @@ dataSource: any[] = [
     cin:  '11093299',
     name: 'nawel',
     type: 'teacher',
-    cv: '.pdf',
+    cv: 'cv3.pdf',
     createdDate: '02/03/2021'
   },
   {
@@ -36,7 +36,7 @@ dataSource: any[] = [
     cin:  '11093299',
     name: 'ayman',
     type: 'teacher',
-    cv: '.pdf',
+    cv: 'cv4.pdf',
     createdDate: '02/03/2021'
   },
   {
@@ -44,7 +44,7 @@ dataSource: any[] = [
     cin:  '11093299',
     name: 'hedi',
     type: 'teacher',
-    cv: '.pdf',
+    cv: 'cv5.pdf',
     createdDate: '02/03/2021'
   }
 ];
