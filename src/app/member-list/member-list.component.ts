17491b042b376567f88fde8d57ae2790082ba43c
StyKon <76910363+StyKon@users.noreply.github.com>
2021-03-09 14:59:23 +0100

    StyKonCommit

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GLOBAL } from '../app-config';
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
@@ -6,48 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
   displayedColumns: string[] = ['id', 'cin', 'name', 'type', 'cv' , 'createdDate'];
-dataSource: any[] = [
-  {
-    id: '123',
-    cin:  '11093299',
-    name: 'khalil',
-    type: 'teacher',
-    cv: 'cv1.pdf',
-    createdDate: '02/03/2021'
-  },
-  {
-    id: '123',
-    cin:  '11093299',
-    name: 'khalil',
-    type: 'teacher',
-    cv: 'cv2.pdf',
-    createdDate: '02/03/2021'
-  },
-  {
-    id: '123',
-    cin:  '11093299',
-    name: 'nawel',
-    type: 'teacher',
-    cv: 'cv3.pdf',
-    createdDate: '02/03/2021'
-  },
-  {
-    id: '123',
-    cin:  '11093299',
-    name: 'ayman',
-    type: 'teacher',
-    cv: 'cv4.pdf',
-    createdDate: '02/03/2021'
-  },
-  {
-    id: '123',
-    cin:  '11093299',
-    name: 'hedi',
-    type: 'teacher',
-    cv: 'cv5.pdf',
-    createdDate: '02/03/2021'
-  }
-];
+dataSource: any[] = GLOBAL._DB.members;
   constructor() { }
 
   ngOnInit(): void {
